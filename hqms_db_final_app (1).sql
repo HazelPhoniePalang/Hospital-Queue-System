9dc40537071946903ed6ba6bf17d9a46d12b08a2
Hazel Phonie Palang <h.palang.535351@umindanao.edu.ph>
2026-05-21 17:30:46 +0800

    Add files via upload

---
+++ hqms_db_final_app (1).sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2026 at 11:30 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hqms_db_final_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+-- Error reading structure for table hqms_db_final_app.cache: #1932 - Table &#039;hqms_db_final_app.cache&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.cache: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`cache`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+-- Error reading structure for table hqms_db_final_app.cache_locks: #1932 - Table &#039;hqms_db_final_app.cache_locks&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.cache_locks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`cache_locks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `counters`
+--
+-- Error reading structure for table hqms_db_final_app.counters: #1932 - Table &#039;hqms_db_final_app.counters&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.counters: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`counters`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+-- Error reading structure for table hqms_db_final_app.departments: #1932 - Table &#039;hqms_db_final_app.departments&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.departments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`departments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `done_assigning`
+-- (See below for the actual view)
+--
+CREATE TABLE `done_assigning` (
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table hqms_db_final_app.failed_jobs: #1932 - Table &#039;hqms_db_final_app.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+-- Error reading structure for table hqms_db_final_app.jobs: #1932 - Table &#039;hqms_db_final_app.jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+-- Error reading structure for table hqms_db_final_app.job_batches: #1932 - Table &#039;hqms_db_final_app.job_batches&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.job_batches: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`job_batches`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table hqms_db_final_app.migrations: #1932 - Table &#039;hqms_db_final_app.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table hqms_db_final_app.password_reset_tokens: #1932 - Table &#039;hqms_db_final_app.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+-- Error reading structure for table hqms_db_final_app.patients: #1932 - Table &#039;hqms_db_final_app.patients&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.patients: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`patients`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+-- Error reading structure for table hqms_db_final_app.payments: #1932 - Table &#039;hqms_db_final_app.payments&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`payments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queues`
+--
+-- Error reading structure for table hqms_db_final_app.queues: #1932 - Table &#039;hqms_db_final_app.queues&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.queues: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`queues`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queue_counters`
+--
+-- Error reading structure for table hqms_db_final_app.queue_counters: #1932 - Table &#039;hqms_db_final_app.queue_counters&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.queue_counters: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`queue_counters`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+-- Error reading structure for table hqms_db_final_app.roles: #1932 - Table &#039;hqms_db_final_app.roles&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`roles`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+-- Error reading structure for table hqms_db_final_app.services: #1932 - Table &#039;hqms_db_final_app.services&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.services: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`services`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+-- Error reading structure for table hqms_db_final_app.sessions: #1932 - Table &#039;hqms_db_final_app.sessions&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.sessions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`sessions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscriptions`
+--
+-- Error reading structure for table hqms_db_final_app.subscriptions: #1932 - Table &#039;hqms_db_final_app.subscriptions&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.subscriptions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`subscriptions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription_items`
+--
+-- Error reading structure for table hqms_db_final_app.subscription_items: #1932 - Table &#039;hqms_db_final_app.subscription_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.subscription_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`subscription_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table hqms_db_final_app.users: #1932 - Table &#039;hqms_db_final_app.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visits`
+--
+-- Error reading structure for table hqms_db_final_app.visits: #1932 - Table &#039;hqms_db_final_app.visits&#039; doesn&#039;t exist in engine
+-- Error reading data for table hqms_db_final_app.visits: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hqms_db_final_app`.`visits`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `done_assigning`
+--
+DROP TABLE IF EXISTS `done_assigning`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `done_assigning`  AS SELECT `queues`.`id` AS `id`, `queues`.`queue_no` AS `queue_no`, `queues`.`symptoms` AS `symptoms`, `queues`.`status` AS `status` FROM `queues` WHERE `queues`.`status` = 'assigned_to_doctor' ;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
